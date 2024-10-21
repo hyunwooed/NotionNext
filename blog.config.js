f4b60e09b6ab73ba58d2a8f64563662647b0a47b
Hyunwoo Jo <hjo6@gmu.edu>
2024-10-21 18:02:25 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Hyunwoo Jo', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'My name is Hyunwoo Jo (조현우), a student majoring in Global Affairs at George Mason University Korea.', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://hyunwoo.org', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.hyunwoo.org', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
